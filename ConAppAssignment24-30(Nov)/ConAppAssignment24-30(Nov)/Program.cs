b3e455294706e0eb67009f0442d9905a916ca30f
AshwiniRajashekar <ashwinirajashekar1111@gmail.com>
2023-11-30 15:27:24 +0530

    Assignment24-30(Nov)

---
+++ ConAppAssignment24-30(Nov)/ConAppAssignment24-30(Nov)/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Reflection;
+class Source
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+}
+
+class Destination
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public string AdditionalProperty { get; set; }
+}
+
+
+
+
+class Program
+{
+    static void MapProperties(object source, object destination)
+    {
+        var sourceType = source.GetType();
+        var destinationType = destination.GetType();
+
+        foreach (var destProp in destinationType.GetProperties())
+        {
+            var sourceProp = sourceType.GetProperty(destProp.Name);
+
+            if (sourceProp != null && sourceProp.PropertyType == destProp.PropertyType)
+            {
+                var value = sourceProp.GetValue(source);
+                destProp.SetValue(destination, value);
+            }
+        }
+    }
+
+    static void Main()
+    {
+        Source sourceInstance = new Source
+        {
+            Id = 1,
+            Name = "Example",
+            Price = 19.99
+        };
+
+        Destination destinationInstance = new Destination();
+
+        MapProperties(sourceInstance, destinationInstance);
+
+        Console.WriteLine("Mapped Properties:");
+        Console.WriteLine($"Id: {destinationInstance.Id}");
+        Console.WriteLine($"Name: {destinationInstance.Name}");
+        Console.WriteLine($"Price: {destinationInstance.Price}");
+        Console.WriteLine($"AdditionalProperty: {destinationInstance.AdditionalProperty}");
+    }
+}
